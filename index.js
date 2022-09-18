1f4e4931cfebdf54403446e10b07ae8b6e4d7711
abhishekchaurasiya <abhichek2580@gmail.com>
2022-09-18 19:17:12 +0530

    completed

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.use("/public", express.static(path.join(__dirname, "public")));
+app.use("/myvideos", express.static(path.join(__dirname, "myvideos")));
 app.use("/", router)
 
 
